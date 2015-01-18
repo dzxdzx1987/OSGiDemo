5145369ee17c3199269e2d7670e74d2178a5bf33
Dong Zhenxin <dzxdzx1987@gmail.com>
2015-01-18 12:06:38 +0800

    create first demo repository

---
+++ com.example.e4.rcp.model/src/com/example/e4/rcp/todo/model/ITodoService.java
@@ -0,0 +1,10 @@
+package com.example.e4.rcp.todo.model;
+
+import java.util.List;
+
+public interface ITodoService {
+	Todo getTodo(long id);
+	boolean saveTodo(Todo todo);
+	boolean deleteTodo(long id);
+	List<Todo> getTodos();
+}
